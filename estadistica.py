e2cd7c827fa834704a2e66bc9495dc676ece5b7f
AlanMondra <a01734565@itesm.mx>
2020-10-28 20:49:30 +0000

    guardando cambios

--- estadistica.py
+++ estadistica.py
@@ -1,20 +1,19 @@
-import csv
-import statistics
+import pandas as pd
 
-file_ = open('insurance.csv')
-lBMI = []
-lAGE = []
-reader = csv.DictReader(file_)
-cnt = 0
-for line in reader:
-    lAGE.append(line['age'])
-    lBMI.append(line['bmi'])
-    cnt += 1
-    break
-print('-----AGE------\nMODA:',statistics.mode(lAGE))
-print('-----AGE------\nMEDIANA:',statistics.median(lAGE))
-#print('-----AGE------\nMEDIA:',statistics.mean(lAGE))
-print('\n-----BMI------\nMODA:',statistics.mode(lBMI))
-print('-----BMI------\nMEDIANA:',statistics.median(lBMI))
-#print('-----BMI------\nMEDIA:',statistics.mean(lBMI))
+df = pd.read_csv('insurance.csv')
 
+df.info()
+df.describe()
+print('\n----------------------------MODE----------------------------\n')
+print(df.mode())
+print('\n------------MEAN------------\n')
+print(df.mean())
+print('\n----------MEDIAN---------\n')
+print(df.median())
+#print('\n-----AGE-----\n',df['age'].mode())
+#print('\n',df['age'].mean())
+#print('\n',df['age'].median())
+
+#print('\n-----BMI-----\n',df['bmi'].mode())
+#print('\n',df['bmi'].mean())
+#print('\n',df['bmi'].median())
